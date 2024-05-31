568bf381df06f759d3f7ebd2457b3b05516b30db
krbp <165799825+krbp@users.noreply.github.com>
2024-05-31 11:09:49 +0200

    Creation du projet + Page login

---
+++ FairFunds/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FairFunds.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
