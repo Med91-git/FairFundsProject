02d84a1dcc71e8be2956bbdd4c9f45b24535847c
Mederic <kottomouyema.m@gmail.com>
2024-06-06 15:57:19 +0200

    bugfix/AjoutCatégorie : Modification de l'héritage de la classe ApplicationDBContext

--- FairFunds/Data/ApplicationDbContext.cs
+++ FairFunds/Data/ApplicationDbContext.cs
@@ -4,10 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FairFunds.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Categorie> Categories { get; set; }
-        public DbSet<Depense> Depenses { get; set; }  
+        public DbSet<Depense> Depenses { get; set; }   
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
