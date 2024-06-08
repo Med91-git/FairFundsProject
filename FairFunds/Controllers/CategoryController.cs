7f08dbf2e3c133096a42ea98defa325e3a2d7ae1
Mederic <kottomouyema.m@gmail.com>
2024-06-08 22:27:54 +0200

    feature/AjoutCatégorie : Suppression des vues sur erreur manip génération automatique des vues CRUD

---
+++ FairFunds/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using FairFunds.Data;
+using FairFunds.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FairFunds.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View(); 
+        }
+    }
+}
