fefeeddbcccb67e8ac74e42e04abded7dd146457
sprinkler515 <165801795+sprinkler515@users.noreply.github.com>
2024-06-14 12:18:59 +0200

    Turn off user's email verification for now

--- FairFunds/Program.cs
+++ FairFunds/Program.cs
@@ -17,7 +17,7 @@ namespace FairFunds
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<CustomUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<CustomUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
  
             builder.Services.AddControllersWithViews();
