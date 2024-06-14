7c6e744533165e333425452b0397e88388077aae
krbp <165799825+krbp@users.noreply.github.com>
2024-06-14 09:57:53 +0200

    Test

---
+++ FairFunds/Controllers/AddExpenditureController.cs
@@ -0,0 +1,51 @@
+﻿using FairFunds.Data;
+using FairFunds.Models;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+using Microsoft.AspNetCore.Identity;
+
+namespace FairFunds.Controllers
+{
+    public class AddExpenditureController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+
+        public AddExpenditureController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/[controller]/AddExpenditure")]
+        public IActionResult AddExpenditure(Expenditure expenditure)
+        {
+            List<CustomUser> users = _context.Users.ToList();
+            List<Category> categories = _context.Categories.ToList();
+            ViewBag.Users = users;
+            ViewBag.Categories = categories;
+            if (ModelState.IsValid)
+            {
+                _context.Expenditures.Add(expenditure);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [Route("/[controller]/Index")]
+        public IActionResult Index()
+        {
+            List<Expenditure> expenditureList = _context.Expenditures.Select((expenditure) => expenditure).ToList();
+
+            return View(expenditureList);
+        }
+
+
+
+
+
+
+
+    }
+}
