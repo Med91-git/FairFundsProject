032179d0eb88ba594931b1f399582815c49f641e
Mederic <kottomouyema.m@gmail.com>
2024-06-21 12:51:25 +0200

    Mise à jour Classe Dépense : ajout des clés etrangères de catégorie et customUser

--- FairFunds/Models/Expenditure.cs
+++ FairFunds/Models/Expenditure.cs
@@ -1,13 +1,27 @@
-﻿namespace FairFunds.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FairFunds.Models
 {
     public class Expenditure
     {
         public int ExpenditureID { get; set; }
+
         public string Title { get; set; }
-        public DateOnly Date { get; set; }
-        public string Debtor { get; set; }
-        public List<string> Creditors { get; set; }
+        public DateOnly Date { get; set; } 
         public int Amount {  get; set; }
-        public Category? Category { get; set; } 
+
+        [ForeignKey("CategoryID")]
+
+        public Category Category { get; set; }
+
+        public int CategoryID { get; set; }
+
+        [ForeignKey("customuserId")]
+        public CustomUser CustomUser { get; set; }
+
+        public string customuserId { get; set; }
+
+
     }
 }
